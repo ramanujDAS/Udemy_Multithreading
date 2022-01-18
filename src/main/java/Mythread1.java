fc79f71eccc85b6f1bed048c1ae7a24c18f919fb
ramanujDAS <ramanuj3rd@gmail.com>
2022-01-18 14:31:30 +0530

    changed=s

--- src/main/java/Mythread1.java
+++ src/main/java/Mythread1.java
@@ -1,2 +1,17 @@
 public class Mythread1 {
+
+    public static void main(String[] args) {
+
+
+        Thread thread  = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from "+ Thread.currentThread().getName());
+            }
+        });
+
+
+        thread.start();
+
+    }
 }
