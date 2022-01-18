99cfc885ca6527b58726c73deacf98d7dd1c59a2
ramanujDAS <ramanuj3rd@gmail.com>
2022-01-19 00:34:06 +0530

    interuppt

--- src/main/java/Mythread1.java
+++
@@ -1,8 +0,0 @@
-public class Mythread1 {
-
-
-    public static void main(String[] args) {
-
-        System.out.print("chekcing");
-    }
-}
--- src/main/java/thread/interept/BlockingTask.java
+++ src/main/java/thread/interept/BlockingTask.java
@@ -1,19 +1,11 @@
 package thread.interept;
 
-import java.math.BigInteger;
-
 class Mythread1 {
 
-    public static void main(String[] args) throws InterruptedException {
 
+    public static void main(String[] args) {
 
-        Thread thread  = new Thread(new BlockingTask() );
-
-        Thread thread1 = new Thread(new Compute(new BigInteger("7"),new BigInteger("100000000")));
-
-        thread1.start();
-        Thread.sleep(1);
-        thread1.interrupt();
+        System.out.print("chekcing");
     }
 }
 
@@ -23,12 +15,6 @@ public class BlockingTask implements Runnable{
 
     @Override
     public void run() {
-
-        try {
-            Thread.sleep(50000);
-        } catch (InterruptedException e) {
-            System.out.print("interrupt occured"+e.getMessage());
-        }
 
     }
 }
